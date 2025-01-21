c035e7424d5681ae32aa17223e919f1e90043b8c
Yhumi0210 <buillaura0210@gmail.com>
2025-01-21 18:33:40 +0100

    feat: menu burger

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -7,6 +7,16 @@ export default function Header() {
     const location = useLocation()
     const navigate = useNavigate()
     const [menuOpen, setMenuOpen] = useState(false)
+    const [isAnimating, setIsAnimating] = useState(false)
+
+    const handleMenuToggle = () => {
+        setIsAnimating(true)
+        setMenuOpen(!menuOpen)
+    }
+
+    const handleAnimationEnd = () => {
+        setIsAnimating(false)
+    }
 
     const handleNavigation = (anchor: string) => {
         if (location.pathname === '/') {
@@ -19,6 +29,7 @@ export default function Header() {
             // Sinon, navigue vers l'accueil avec l'ancre
             navigate(`/#${anchor}`)
         }
+        setMenuOpen(false)
     }
 
     const isActive = (path: string) => {
@@ -27,26 +38,37 @@ export default function Header() {
         return false
     }
 
+    // const navClasses = `hero__nav ${menuOpen ? 'is-open' : isAnimating ? 'is-closed' : ''}`
+
     return (
-        <div className='header'>
+        <header className='header'>
             <Link to='' className='header__logo'>
                 <img src={logo} alt='Logo'/>
             </Link>
-            <div>
-                <button className='menu-toggle' onClick={() => setMenuOpen(!menuOpen)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="currentColor" className="size-6 white">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/>
-                    </svg>
-                </button>
-                <div className={`nav ${menuOpen ? 'nav-open' : ''}`}>
-                    <Link to='' className={`nav__link ${isActive('/') ? 'active' : ''}`}>accueil</Link>
-                    <p onClick={() =>  {handleNavigation('projets'); setMenuOpen(false)}}
-                       className={`nav__link ${isActive('projets') ? 'active' : ''}`}>projets</p>
-                    <p onClick={() => {handleNavigation('contact'); setMenuOpen(false)}}
-                       className={`nav__link ${isActive('contact') ? 'active' : ''}`}>contact</p>
-                </div>
-            </div>
-        </div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                 stroke="currentColor" className="menu size-6 white" onClick={handleMenuToggle}>
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/>
+            </svg>
+            <nav className={`nav ${menuOpen ? 'is-open' : isAnimating ? 'is-closed' : ''}`}
+                 onAnimationEnd={handleAnimationEnd}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                     stroke="currentColor" className="menu-toggle size-6 white" onClick={handleMenuToggle}>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5"/>
+                </svg>
+                {/*<div className={`nav ${menuOpen ? 'nav-open' : ''}`}>*/}
+                <Link to='' className={`nav__link ${isActive('/') ? 'active' : ''}`}>accueil</Link>
+                <p onClick={() => {
+                    handleNavigation('projets');
+                    setMenuOpen(false)
+                }}
+                   className={`nav__link ${isActive('projets') ? 'active' : ''}`}>projets</p>
+                <p onClick={() => {
+                    handleNavigation('contact');
+                    setMenuOpen(false)
+                }}
+                   className={`nav__link ${isActive('contact') ? 'active' : ''}`}>contact</p>
+                {/*</div>*/}
+            </nav>
+        </header>
     )
 }
